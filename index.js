4352be4fb13db454cd624774dfde178f9a5472ee
FredP21 <f.pelletier21@gmail.com>
2023-10-03 11:06:49 +0200

    i use chalk module

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+const myHomeBoy = "Alhassane"
+const mySecondHomeBoy = "Charles"
+const myName = "Frédéric"
+const substitute = "Benoît or Christopher :)"
+
+console.log(chalk.blue(myHomeBoy));
+console.log(chalk.red(mySecondHomeBoy));
+console.log(chalk.green(myName));
+console.log(chalk.yellow(substitute));
